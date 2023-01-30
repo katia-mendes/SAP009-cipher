cf62f8107a63c991fff73cfd163a513374e7f17c
katia-mendes <katia.mendesx@gmail.com>
2023-01-29 23:38:29 -0300

    novas alterações do projeto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,51 @@
 const cipher = {
-  // ...
+  encode(deslocamento, mensagem) { 
+    let fraseFinal = [];
+    let charDesloc = '';
+    let charCipher = '';
+
+    for(let letra in mensagem) {
+      let char = mensagem.charCodeAt(letra);
+      char = parseInt(char);
+      while (deslocamento < 0) {
+        deslocamento = deslocamento + 26; 
+      }
+      if (char >= 65 && char <= 90) {
+        charDesloc = (((char - 65) + deslocamento) % 26) = 65;
+        charCipher = String.fromCharCode(charDesloc);
+        fraseFinal.push(charCipher);
+
+      } else {
+        charCipher = String.fromCharCode(char);
+        fraseFinal.push(charCipher);
+      }
+    }
+  return fraseFinal.join('');
+  },
+
+  decode(deslocamento, mensagem) { 
+    let fraseFinal = [];
+    let charDesloc = '';
+    let charCipher = '';
+
+    for(let letra in mensagem) {
+      let char = mensagem.charCodeAt(letra);
+      char = parseInt(char);
+      while (deslocamento < 0) {
+        deslocamento = deslocamento + 26; 
+      }
+      if (char >= 65 && char <= 90) {
+        charDesloc = (((char - 90) + deslocamento) % 26) = 90;
+        charCipher = String.fromCharCode(charDesloc);
+        fraseFinal.push(charCipher);
+
+      } else {
+        charCipher = String.fromCharCode(char);
+        fraseFinal.push(charCipher);
+      }
+    }
+  return fraseFinal.join('');
+  } 
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import cipher from './cipher.js';
+import cipher from "cipher.js";
 
-console.log(cipher);
+let criptografia = (categoria) => { 
+    let deslocamento = parseInt(document.getElementById("deslocamento").value);
+    let mensagem = document.getElementById("mensagem").value;
+    let resultado = '';
+    if(categoria == 'cifrar'){ 
+        resultado = cipher.encode(deslocamento, mensagem); 
+    } else{
+        resultado = cipher.decode(deslocamento, mensagem); 
+    }
+    document.getElementById('resultado').innerHTML = resultado;
+}
+console.log(criptografia('cifrar'));
+
+
+
+
+
